70dc6535c945174f23837c01d9e45d229ed531b8
valu_cmd <vale.alvarado901@gmail.com>
2024-05-08 02:41:21 -0600

    Corrigiendo responsive del navbar

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 // Selección de elementos
 let cartIcon = document.querySelector('#cart-icon');
+let cartIconM = document.querySelector('#mobile i');
 let cart = document.querySelector('.cart');
 let closeCart = document.querySelector('#close-cart');
 
 // Abrir el carrito al hacer clic en el ícono del carrito
-cartIcon.addEventListener('click', () => {
+cartIcon.onclick = () => {
     cart.classList.toggle('active');
-});
+};
+
+// Abrir el carrito al hacer clic en el ícono del carrito en responsive
+cartIconM.onclick = () => {
+    cart.classList.toggle('active');
+};
 
 // Cerrar el carrito al hacer clic en el botón de cierre
-closeCart.addEventListener('click', () => {
+closeCart.onclick = () => {
     cart.classList.remove('active');
-});
+};
