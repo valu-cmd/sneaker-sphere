6d6191e34a97609309e917bbb910f35eda9ef73a
valu_cmd <vale.alvarado901@gmail.com>
2024-05-13 16:40:17 -0600

    Agregando funcion de abrir y cerrar carrito

--- public/js/script.js
+++ public/js/script.js
@@ -14,3 +14,24 @@ if(close){
         nav.classList.remove('active');
     })
 }
+
+// Selección de elementos
+let cartIcon = document.querySelector('#cart-icon');
+let cartIconM = document.querySelector('#mobile i');
+let cart = document.querySelector('.cart');
+let closeCart = document.querySelector('#close-cart');
+
+// Abrir el carrito al hacer clic en el ícono del carrito
+cartIcon.onclick = () => {
+    cart.classList.toggle('active');
+};
+
+// Abrir el carrito al hacer clic en el ícono del carrito en responsive
+cartIconM.onclick = () => {
+    cart.classList.toggle('active');
+};
+
+// Cerrar el carrito al hacer clic en el botón de cierre
+closeCart.onclick = () => {
+    cart.classList.remove('active');
+};
